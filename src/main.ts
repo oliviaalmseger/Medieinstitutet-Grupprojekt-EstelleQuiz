914154004c3831527f96dd6503770e2537a4e2b0
TeaGross <tea.grossman@gmail.com>
2024-12-18 16:56:15 +0100

    feat(landing page): variable for username & clickevent for start gamebtn
    -variable for input.value
    -clickevent for start-game button
    -hide landing-page when start-gamebtn is clicked
    -username cant be left blank, else alert

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,39 @@ import quizQuestionsArray from "./quizArray.ts";
 quizQuestionsArray.forEach(index => {
     console.log(index.options);
 });
+
+
+/*************************** Landing page ****************************/
+//variabel för username
+let username;
+// hämtar inputfält och start game btn
+const usernameInput = document.querySelector('#usernameInput') as HTMLInputElement;
+const startGameBtn = document.querySelector('#startGameBtn') as HTMLButtonElement;
+
+// klickevent på starta spel
+startGameBtn.addEventListener('click', startGame);
+
+function startGame() {
+    // När username är ifyllt kan spelet starta, annars return
+    if (usernameInput) {
+        username = usernameInput.value;
+      if(username.length > 0) {
+          // Hämta värdet och spara i en variabel
+          console.log('Användarnamn:', username);
+
+            const landingPage = document.querySelector('#landingPage') as HTMLElement;
+            landingPage.classList.add('hidden');
+
+        // funktion: starta + visa timer
+        // funktion: Starta poängräkning
+        // funktion: starta + visa frågeräkning
+        // funktion: visa frågor + svarsalternativ
+         
+      } 
+      else {
+        alert("Du måste ange ett användarnamn!");
+        return;
+      }
+
+    } 
+}
