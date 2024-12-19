86ab495b1cb8baf667c00ac66d95d6babbc7110c
TeaGross <tea.grossman@gmail.com>
2024-12-19 13:21:06 +0100

    feat(landing-page): changes after feedback

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ quizQuestionsArray.forEach(index => {
 /*************************** Landing page ****************************/
 //variabel för username
 let username;
+const landingPage = document.querySelector('#landingPage') as HTMLElement;
+
 // hämtar inputfält och start game btn
 const usernameInput = document.querySelector('#usernameInput') as HTMLInputElement;
 const startGameBtn = document.querySelector('#startGameBtn') as HTMLButtonElement;
@@ -17,26 +19,19 @@ const startGameBtn = document.querySelector('#startGameBtn') as HTMLButtonElemen
 startGameBtn.addEventListener('click', startGame);
 
 function startGame() {
-    // När username är ifyllt kan spelet starta, annars return
-    if (usernameInput) {
-        username = usernameInput.value;
-      if(username.length > 0) {
-          // Hämta värdet och spara i en variabel
-          console.log('Användarnamn:', username);
 
-            const landingPage = document.querySelector('#landingPage') as HTMLElement;
-            landingPage.classList.add('hidden');
-
-        // funktion: starta + visa timer
-        // funktion: Starta poängräkning
-        // funktion: starta + visa frågeräkning
-        // funktion: visa frågor + svarsalternativ
-         
-      } 
-      else {
-        alert("Du måste ange ett användarnamn!");
+    username = usernameInput.value;
+    if (username.length === 0) {
+        alert('Du måste ange ett användarnamn!');
         return;
-      }
+    }
+   
+    console.log('Användernamn: ' + username);
+ 
+    landingPage.classList.add('hidden');
 
-    } 
+    // funktion: starta + visa timer
+    // funktion: Starta poängräkning
+    // funktion: starta + visa frågeräkning
+    // funktion: visa frågor + svarsalternativ
 }
